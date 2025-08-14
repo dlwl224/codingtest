6407bf64543b0a18936bd6cc24c274be26b11770
dlwl224 <144751705+dlwl224@users.noreply.github.com>
2025-08-14 10:22:06 +0900

    Title: 비교 연산, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250814/비교 연산/comparison-operator.py
@@ -0,0 +1,26 @@
+A, B=input().split()
+
+if A>=B:
+    print(1)
+else:
+    print(0)
+if A>B:
+    print(1)
+else:
+    print(0)
+if A<=B:
+    print(1)
+else:
+    print(0)
+if A<B:
+    print(1)
+else:
+    print(0)
+if A==B:
+    print(1)
+else:
+    print(0)
+if A!=B:
+    print(1)
+else:
+    print(0)
