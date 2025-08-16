77e8119e9e6288f985f77026ca1c851458b6b617
dlwl224 <144751705+dlwl224@users.noreply.github.com>
2025-08-16 23:22:49 +0900

    Title: 윤년인가, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250816/윤년인가/is-leap-year.java
+++ 250816/윤년인가/is-leap-year.java
@@ -3,17 +3,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int y=sc.nextInt();
-        if(y%4==0){
-            if(y%100==0 &&  y%400==0){
-                System.out.print("false");
+        if(y % 4 == 0) {
+            if(y % 100 == 0) {
+                if(y % 400 == 0) {
+                    System.out.println("true");
+                }
+                else {
+                    System.out.println("false");
+                }
             }
-            else{
-                System.out.print("true");
+            else {
+                System.out.println("true");
             }
         }
-        else{
-            System.out.print("false");
-
+        else {
+            System.out.println("false");
         }
     }
 }
