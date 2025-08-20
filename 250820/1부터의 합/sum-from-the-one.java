64646ace79a6213517703b6fce125b774da8dcc7
dlwl224 <144751705+dlwl224@users.noreply.github.com>
2025-08-20 16:08:41 +0900

    Title: 1부터의 합, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250820/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int sum=0;
+        for(int i=1; sum<=n; i++){
+            sum+=i;
+            if(sum>=n){
+                System.out.print(i);
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+}
